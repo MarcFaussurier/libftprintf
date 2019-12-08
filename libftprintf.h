fde5f3309dede4d04f27730fa49a9db94fb4df1e
Marc Faussurier <mfaussur@z4r3p5.le-101.fr>
2019-12-08 08:39:56 +0100

    init

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   libftprintf.h                                    .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mfaussur <mfaussur@student.le-101.>        +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/04 13:49:03 by mfaussur     #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/04 13:49:54 by mfaussur    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int ft_printf(const char *, ...);
+
+#endif

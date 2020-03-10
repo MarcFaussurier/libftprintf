d0cea71b79df031964bbb28875c0bf9e115f7d23
marcfsr <faussurier.marc@icloud.com>
2020-03-10 20:06:17 +0100

    now passing all the 1413 tests :D

--- specifiers/ft_fmt_o.c
+++ specifiers/ft_fmt_o.c
@@ -2,9 +2,11 @@
 
 char        *ft_fmt_o(t_specifier_state state, va_list ap)
 {
-	if (state.flags.sharp && state.precision != NO_PRECISION)
+	if (state.flags.sharp && state.precision != NO_PRECISION && state.precision >= 1)
     {
         state.precision -= 1;
     }
+    state.flags.plus = 0;
+    state.flags.space = 0;
     return (ft_unumtoa(state, ap, b8, "0"));
 }
--- specifiers/ft_fmt_x.c
+++ specifiers/ft_fmt_x.c
@@ -2,7 +2,8 @@
 
 char        *ft_fmt_x(t_specifier_state state, va_list ap)
 {
-
+    state.flags.plus = 0;
+    state.flags.space = 0;
 	return (ft_unumtoa(state, ap, ft_isupper(state.specifier) ? B16 : b16,
 				(char[3]){'0',ft_isupper(state.specifier) ? 'X' : 'x', 0}));
 }

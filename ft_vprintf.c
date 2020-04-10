227c5fe5e38549ab1688df988e9eb21b78fa6bae
marcfsr <faussurier.marc@icloud.com>
2020-04-10 10:10:10 +0200

    Fixing wrong vprintf fd... this project can now be tested with all community testers

--- ft_vprintf.c
+++ ft_vprintf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_vprintf.c                                     .::    .:/ .      .::   */
+/*   ft_vprintf.c                                       :+:      :+:    :+:   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: mfaussur <mfaussur@student.le-101.>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/02/05 08:31:04 by mfaussur     #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/05 10:07:27 by mfaussur    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/04/10 10:04:22 by mfaussur         ###   ########lyon.fr   */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,5 +15,5 @@
 
 int				ft_vprintf(const char *fmt, va_list ap)
 {
-    return (ft_vdprintf(STDIN_FILENO, fmt, ap));
+    return (ft_vdprintf(STDOUT_FILENO, fmt, ap));
 }

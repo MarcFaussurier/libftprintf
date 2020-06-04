d7b164184b0d535e4ee02391131b8c818dd77882
marcfsr <faussurier.marc@icloud.com>
2020-06-04 19:10:43 +0200

    removing an obsoltete test file

--- sources/ft_read_state_ld.c
+++ sources/ft_read_state_ld.c
@@ -13,14 +13,12 @@ t_ld_state                  ft_read_state_ld(long double x)
 			return FP_SUBNORMAL;
 		return FP_ZERO;
 	}
-
     if (i == -1)
 	{
 		if (xp->s_parts.u_fraction.value)
             return FP_NAN;
         return FP_MINUS_INFINITY;
 	}
-
     if (i == SHRT_MAX)
 	{
 		if (xp->s_parts.u_fraction.value)
